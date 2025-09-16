8b0a79d1b769c8bb8640aaf4ce67aa37301a700c
rea-19 <r.besa@student.uq.edu.au>
2025-09-16 17:07:36 +1000

    Add files via upload

--- js/get_events.js
+++ js/get_events.js
@@ -1,25 +1,22 @@
-/**
-* Function that iterates over the data returned by the Fetch request
-Things to do:
-* add points depending on the price of the event
-*/
-
 function iterateRecords(data) {
-	// Log the data to the console so that we can see what is being passed to the function
+
 	console.log("Data returned: "+JSON.stringify(data));
-	// Looking at what is returned, we can see that the data records are stored in the results object, which we pull out so we can iterate over each record
+	
 	var records = data.results;
-	//to iterate over each of the values, we use Object.values - passing it the records and then using forEach to look at each value in the object
+
+
 	Object.values(records).forEach(value => {
-		
-		// what other data can we get from the records?
 		var subject = value["subject"];
 		var description = value["description"];
-
 		var location = value["location"];
         var date = value["formatteddatetime"];
         var age = value["age"];
 
+		// Filter out events for young adults, adults, seniors
+        if (!age || /young adult|adult|senior/i.test(age)) {
+            return; // Skip this record
+        }
+		
         // add points and price indicator depending on the cost of the event
         var rawCost = value["cost"];
         var points = 0;
@@ -44,20 +41,26 @@ function iterateRecords(data) {
 
 		// check that we have data for each of the fields we want to display
 		if(subject && description && location && date && age && price && points) {
-				
-				// Use jQuery DOM manipulation to insert a section for each record in the set
-                $("#records").append(
-                    $('<section class="record">').append(
-                        $('<h2>').text(subject),
-                        $('<p>').text(description),
-                        $('<p>').text(location),
-                        $('<p>').text(age),
-                        $('<p>').text(date),
-                        $('<p>').text(price),
-                        $('<p>').text("Points: "+points),
-                        $('<button class="view-more">').text("View More").click(function() {
-                            window.location.href = "/html/event_details.html?event=" + encodeURIComponent(subject);
-                        })
+            $("#records").append(
+                $('<section class="record">').append(
+                    $('<h2>').text(subject),
+                    $('<p>').text(description),
+                    $('<p>').text(location),
+                    $('<p>').text(age),
+                    $('<p>').text(date),
+                    $('<p>').text(price),
+                    $('<p>').text("Points: "+points),
+                    $('<button class="view-more">')
+						.text("View More")
+						.click(function() {
+							const params = new URLSearchParams({
+							subject: subject,
+							start: value["start_datetime"],
+							end: value["end_datetime"]
+							});
+
+							window.location.href = "/html/event_details.html?" + params.toString();
+						})
                     )
                 );
 		}
@@ -65,34 +68,22 @@ function iterateRecords(data) {
 }
 
 
-/** 
-* When the document has loaded, we will call the API and request some data.
-*/
 $(document).ready(function() {
-	var data = {};
-	// set the base URL for the dataset you are connecting to
-	const apiURL = "https://data.brisbane.qld.gov.au/api/explore/v2.1/catalog/datasets/infants-and-toddlers-events/records";
-	
-	// define request parameters that can be sent to refine the results. What other parameters could we send for this dataset?
-	const requestParams = {
-		limit: 50 // the number of records to return
-	}
-	//create a string from the request parameter object that we can add to the end of the API url
-	const queryString = new URLSearchParams(requestParams).toString(); //slightly convoluted way to construct the URL but can help to avoid errors in the text
-	
-	//construct the full API call URl by concatenating the apiURL with the request parameters, the ? indicates there are parameters to come
-	const fullURL = apiURL + "?" + queryString;
-	
-	console.log("URL: " + fullURL); // log the fullURL for error checking
-	
-	/**
-	* Using fetch, we pass it the fullURL to connect to, 
-	* We specify that we want the response to return json - what other response formats can we ask for?
-	* We specify a function to process the data that is returned
-	* We specify how to deal with errors that occur
-	*/
-	fetch(fullURL)
-		.then(response => response.json())
-		.then(data => iterateRecords(data))
-		.catch(error => console.error("Error fetching data:", error));
+	const apiURLs = [
+		"https://data.brisbane.qld.gov.au/api/explore/v2.1/catalog/datasets/infants-and-toddlers-events/records",
+		"https://data.brisbane.qld.gov.au/api/explore/v2.1/catalog/datasets/library-events/records"
+	];
+
+	const requestParams = { limit: 50 };
+	const queryString = new URLSearchParams(requestParams).toString();
+
+	apiURLs.forEach(apiURL => {
+		const fullURL = apiURL + "?" + queryString;
+		console.log("Fetching: " + fullURL);
+
+		fetch(fullURL)
+			.then(response => response.json())
+			.then(data => iterateRecords(data))
+			.catch(error => console.error("Error fetching data from " + apiURL, error));
+	});
 });
