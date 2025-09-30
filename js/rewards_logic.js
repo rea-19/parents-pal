5eaa9e2c85bfbd038a6a21a0838bbacb4bc87171
nikitap108 <n.proud@student.uq.edu.au>
2025-09-30 15:30:20 +1000

    Create rewards_logic.js

---
+++ js/rewards_logic.js
@@ -0,0 +1,2 @@
+//section for both rewards page and rewards system. 
+//this includes the following: navbar progress bar, rewards page display on card, rewards page setting goals section, booking events to get points
