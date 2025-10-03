c167ac42e5c8a18dc54c7a9240c384928ebffa7d
nikitap108 <n.proud@student.uq.edu.au>
2025-10-03 13:01:37 +1000

    Update rewards

--- js/rewards_logic.js
+++ js/rewards_logic.js
@@ -2,3 +2,16 @@
 //this includes the following: navbar progress bar, rewards page display on card, rewards page setting goals section, booking events to get points
 
 //pratise logic - will update later
+const progress = document.querySelector(".progress-done")
+const input = document.querySelector(".input")
+const maxInput = document.querySelector(".maxInput")
+let finalValue = 0
+let max = 0
+
+input.addEventListener("keyup", function () {
+    finalValue = parseInt(input.value, 10);
+});
+
+maxInput.addEventListener("keyup", function () {
+    max = parseInt(maxInput.value, 10);
+});
