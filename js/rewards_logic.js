d770fcb92e48200ba3904f4f13057549152f89f2
nikitap108 <n.proud@student.uq.edu.au>
2025-10-03 12:18:05 +1000

    rewards logic

--- js/rewards_logic.js
+++ js/rewards_logic.js
@@ -1,2 +1,4 @@
 //section for both rewards page and rewards system. 
 //this includes the following: navbar progress bar, rewards page display on card, rewards page setting goals section, booking events to get points
+
+//pratise logic - will update later
